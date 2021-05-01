62462322b80265485334e2a409e706594c026856
TobMit <tobik.m1@gmail.com>
2021-05-01 17:53:11 +0200

    Vytvorenie projektu

---
+++ src/sk/falloutshelter/fri/Main.java
@@ -0,0 +1,14 @@
+package sk.falloutshelter.fri;
+/**
+ * Created by IntelliJ IDEA.
+ * User: Tobias
+ * Date: 1. 5. 2021
+ * Time: 17:51
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO Write your code
+        System.out.println("Hello world");
+    }
+}
