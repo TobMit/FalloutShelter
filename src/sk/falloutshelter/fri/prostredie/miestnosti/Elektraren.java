f2d48b84adc4c559bfc6d148949c564436f99aec
TobMit <tobik.m1@gmail.com>
2021-05-20 21:38:12 +0200

    Koniec dňa Rozrobená elektráreň

--- src/sk/falloutshelter/fri/prostredie/miestnosti/Elektraren.java
+++ src/sk/falloutshelter/fri/prostredie/miestnosti/Elektraren.java
@@ -10,9 +10,14 @@ import java.awt.*;
  * @author Tobias
  */
 public class Elektraren extends sk.falloutshelter.fri.prostredie.miestnosti.Miestnosti {
+
+    //todo dokončiť, Elektráreň a ostané okrem vodárne nedoknčené.
     private final int riadok;
     private final int stlpec;
     private final RozlozenieMiestnosti rozlozenieMiestnosti;
+    private final int xSuradnica;
+    private final int ySuradnica;
+    private final StavMiestnosti stavMiestnosti;
     private int pocetLudi;
     private int sirkaMiestnosti;
 
@@ -21,6 +26,11 @@ public class Elektraren extends sk.falloutshelter.fri.prostredie.miestnosti.Mies
         this.riadok = riadok;
         this.stlpec = stlpec;
         this.rozlozenieMiestnosti = rozlozenieMiestnosti;
+        this.xSuradnica = Bunker.X_SURADNICA_BUNKRA + this.stlpec * RozlozenieMiestnosti.SIRKA_MIESTNOSTI;
+        this.ySuradnica = Bunker.Y_SURADNICA_BUNKRA + this.riadok * RozlozenieMiestnosti.VYSKA_MIESTNOSTI;
+        //this.image = new ImageIcon("src/sk/falloutshelter/fri/obr/Miestnosti/builderMiestnost/builderMiestnost-1.png").getImage();
+
+        this.stavMiestnosti = StavMiestnosti.Pracuje;
         this.pocetLudi = 0;
         this.sirkaMiestnosti = 1;
 
